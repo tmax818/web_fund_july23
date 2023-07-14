fd4d739704e4f1b2e1ecbdee1ab0a8f72f402d3a
tmax818 <tylermaxwell661@gmail.com>
2023-07-14 13:38:56 -0600

    up

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -18,6 +18,7 @@ function dojoCounting(){}
 
 //TODO Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
 
+
 //TODO Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 
 var x = [ [5,2,3], [10,8,9] ] 
