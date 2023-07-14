7efa590134f58f4b62116d3e07bcd762c607c146
tmax818 <tylermaxwell661@gmail.com>
2023-07-14 13:34:49 -0600

    add js classwork

---
+++ JavaScript/index.js
@@ -0,0 +1,27 @@
+// TODO Basic - Print all integers from 0 to 150.
+
+function print0To150(){}
+
+// TODO Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+
+
+// TODO Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+function dojoCounting(){}
+
+
+//TODO Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+
+
+//TODO Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+
+//TODO Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+//TODO Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+
+var x = [ [5,2,3], [10,8,9] ] 
+
+console.log(x)
+
+
