f92f9cf6c51a0f5e75e2d0eca8938238afc353d9
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-07-17 11:54:01 -0600

    add more comments

--- algorithms/day06/random.js
+++ algorithms/day06/random.js
@@ -2,9 +2,9 @@
 
 // The Math Library
 // Predict what each of the following will return. Can we predict the variable random?
-var floor = Math.floor(1.8);
-var ceiling = Math.ceil( Math.PI ); 
-var random = Math.random(); 
+var floor = Math.floor(1.8); // 1
+var ceiling = Math.ceil( Math.PI ); // 3.14.. // 4
+var random = Math.random(); // 0 - 0.99999999
     
 console.log(floor);
 console.log(ceiling);
@@ -28,7 +28,7 @@ console.log(random);
 // Using what we've learned about the Math library in JavaScript, complete the following function that should return a value between 1 through 6 at random.
 
 function d6() {
-    var roll = Math.random(); 
+    var roll = Math.random(); // 0 - 0.999999
     // your code here
     return roll;
 }
