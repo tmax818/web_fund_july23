0b72877e80549e6dbf954f7cdd64821eb2002142
tmax818 <tylermaxwell661@gmail.com>
2023-07-24 08:32:41 -0600

    update day 11 algos

---
+++ algorithms/day11/index.js
@@ -0,0 +1,40 @@
+// TODO: Print all the integers from 1 to 255.
+function print1to255(){}
+
+// TODO: Print integers from 0 to 255, and with each integer print the sum so far.
+function printIntsAndSum0to255(){}
+
+// TODO: Given an array, find and print its largest element.
+function printMaxOfArray(arr){}
+
+// TODO: Create an array with all the odd integers between 1 and 255 (inclusive).
+function returnOddsArray1to255(){}
+
+// TODO: Given an array and a value Y, count and print the number of array values greater than Y.
+function returnArrayCountGreaterThanY(arr, y){}
+
+// TODO: Given an array, print the max, min and average values for that array.
+function printMaxMinAvgArray(arr){}
+
+// TODO: Replace any negative array values with 'Dojo'.
+function swapStringForArrayNeg(arr){}
+
+function swapStringForArrayNeg2(arr){}
+    
+// TODO: Print all odd integers from 1 to 255.
+function printOdds1to255(){}
+
+// TODO: Iterate through a given array, printing each value.
+function printArrayVal(arr){}
+
+// TODO: Analyze an array’s values and print the average
+function printAvgOfArray(arr){}
+
+// TODO: Square each value in a given array, returning that same array with changed values
+function squareArrayVals(arr){}
+
+// TODO: Return the given array, after setting any negative values to zero.
+function zeroOutArrayNegativeVals(arr){}
+
+// TODO: Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+function shiftArrayValsLeft(arr){}
