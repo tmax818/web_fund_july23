7d7506c03f6be9cbff45de32330c112dcc364475
tmax818 <tylermaxwell661@gmail.com>
2023-07-25 08:16:05 -0600

    add comments

--- algorithms/day12/index.js
+++ algorithms/day12/index.js
@@ -12,15 +12,15 @@ var expected1 = "b";
 // after function call, arr1 should be:
 var arr1Expected = ["a", "c", "d", "e"];
 
-var arr2 = ["a", "b", "c"];
-var removeIdx2 = 3;
-var expected2 = null;
-var arr2Expected = ["a", "b", "c"];
+// var arr2 = ["a", "b", "c"];
+// var removeIdx2 = 3;
+// var expected2 = null;
+// var arr2Expected = ["a", "b", "c"];
 
-var arr3 = ["a", "b", "c"];
-var removeIdx3 = -3;
-var expected3 = null;
-var arr3Expected = ["a", "b", "c"];
+// var arr3 = ["a", "b", "c"];
+// var removeIdx3 = -3;
+// var expected3 = null;
+// var arr3Expected = ["a", "b", "c"];
 
 /**
  * Removes and returns the item at the given idx from the given array and
@@ -44,10 +44,10 @@ var result1 = removeAt(arr1, removeIdx1);
 console.log(result1, "should equal", expected1);
 console.log(arr1, "should equal", arr1Expected);
 
-var result2 = removeAt(arr2, removeIdx2);
-console.log(result2, "should equal", expected2);
-console.log(arr2, "should equal", arr2Expected);
+// var result2 = removeAt(arr2, removeIdx2);
+// console.log(result2, "should equal", expected2);
+// console.log(arr2, "should equal", arr2Expected);
 
-var result3 = removeAt(arr3, removeIdx3);
-console.log(result3, "should equal", expected3);
-console.log(arr3, "should equal", arr3Expected);
+// var result3 = removeAt(arr3, removeIdx3);
+// console.log(result3, "should equal", expected3);
+// console.log(arr3, "should equal", arr3Expected);
